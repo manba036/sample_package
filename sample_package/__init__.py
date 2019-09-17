0dadf5270bb148c09e981a256b511490ec9085f6
Manba Osamu <manba@zf7.so-net.ne.jp>
2019-09-18 00:28:49 +0900

    Add files

---
+++ sample_package/__init__.py
@@ -0,0 +1,2 @@
+from .sample_package import sample_text
+from .sample_package import sample_list
---
+++ sample_package/sample_package.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def sample_text():
+    return 'sample'
+
+def sample_list():
+    return [ 'sample', 'package' ]
+
+def main():
+    print(sample_text())
+    print(sample_list())
+
+if __name__ == '__main__':
+    main()

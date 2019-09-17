aefcb5ad946f76116406cf071d55ad9a61492ec4
Manba Osamu <manba@zf7.so-net.ne.jp>
2019-09-18 00:42:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name='sample_package',
     version='0.1',
     description='Sample to pip install via GitHub.',
-    long_description='./README.md',
     author='Manba Osamu',
     author_email='manba@zf7.so-net.ne.jp',
     install_requires=[],
     url='https://github.com/manba036/sample_package',
-    license='./LICENSE',
+    license='MIT',
     packages=find_packages(),
 )
